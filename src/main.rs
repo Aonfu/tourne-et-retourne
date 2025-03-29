d796ab5716694b76f18b219ea0d239ad4d05de56
Aonfu <noafouich.etude@protonmail.com>
2025-03-29 23:56:37 +0800

    main forgotten : (

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use macroquad::prelude::*;
 
-
 #[macroquad::main("tourne-et-retourne")]
 async fn main() {
+    let texture = load_texture("assets/theo.png").await.unwrap();
+    let mut rot = 0.;
     loop {
-        clear_background(SKYBLUE); 
-
+        clear_background(SKYBLUE);
+        rot = rot + 0.1;
+        draw_texture_ex(&texture, 100., 100., WHITE,
+             DrawTextureParams{
+                dest_size: Some(vec2(500., 500.)),
+                rotation: rot,
+                ..Default::default()
+                });
+        if rot >= 6.282 {
+            rot = 0.;
+        }
         next_frame().await; 
     }
 }
