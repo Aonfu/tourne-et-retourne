b9cc580886b223a3afc587c2bff5596962df3066
Aonfu <noafouich.etude@protonmail.com>
2025-04-02 11:57:23 +0800

    clean code, struct Entity

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
 use macroquad::prelude::*;
 
+struct Game {
+}
+struct Entity {
+    coord : Vec2,
+    texture : Option<Texture2D>,
+    rotation : f32,
+    dest_size : Option<Vec2>,
+}
+
+impl Entity {
+    fn draw(&mut self){
+        self.rotation = self.rotation % 6.28;
+        let draw_param = DrawTextureParams{
+            dest_size: self.dest_size,
+            rotation : self.rotation,
+            ..Default::default()
+        };
+        // we use let Some ... because else we have to use clone to have texture in the function
+        if let Some(texture) = &self.texture {
+            draw_texture_ex(texture, self.coord.x, self.coord.y, WHITE, draw_param);
+        }
+    }
+}
+impl Default for Entity{
+    fn default() -> Self {
+        Entity {
+            coord : Vec2 {x: 0., y: 0.},
+            texture : None,
+            rotation : 0.,
+            dest_size : None,
+        }
+    }
+}
+
 #[macroquad::main("tourne-et-retourne")]
 async fn main() {
-    let texture = load_texture("assets/theo.png").await.unwrap();
-    let mut rot = 0.;
+    let mut theo = Entity {
+        texture : Some(load_texture("assets/theo.png").await.unwrap()),
+        dest_size: Some(vec2(500., 500.)),
+        ..Default::default()
+    };
     loop {
         clear_background(SKYBLUE);
-        rot = rot + 0.1;
-        draw_texture_ex(&texture, 100., 100., WHITE,
-             DrawTextureParams{
-                dest_size: Some(vec2(500., 500.)),
-                rotation: rot,
-                ..Default::default()
-                });
-        if rot >= 6.282 {
-            rot = 0.;
-        }
+        theo.rotation = theo.rotation + 0.1;
+        theo.draw();
         next_frame().await; 
     }
 }
