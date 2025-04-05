6046e878d9a6d7e3a1aeac66f39599ee797e0611
Aonfu <noafouich.etude@protonmail.com>
2025-04-05 15:07:02 +0800

    parsing ldtk file in rust

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,109 @@
 use macroquad::prelude::*;
+use serde::Deserialize;
+use serde_json::{json,Value};
 
-struct Entity<'a> {
-    coord : Vec2,
-    texture : &'a Texture2D,
-    rotation : f32,
-    dest_size : Option<Vec2>,
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Refind Maho".to_string(),
+        window_width: 640,
+        window_height: 320,
+        fullscreen: false,
+        ..Default::default()
+    }
 }
 
-impl Entity {
+#[derive(Deserialize, Debug)]
+struct LDtkProject {
+    levels: Vec<LDtkLevel>,
+}
+
+#[derive(Deserialize, Debug)]
+struct LDtkLevel {
+    identifier: String,
+    #[serde(rename = "layerInstances")]
+    layer_instances: Option<Vec<LDtkLayer>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct LDtkLayer {
+    #[serde(rename = "__identifier")]
+    identifier: String,
+
+    #[serde(rename = "gridTiles")]
+    tiles: Vec<LDtkTile>,
+}
+
+#[derive(Deserialize, Debug)]
+struct LDtkTile {
+    #[serde(rename = "px")]
+    position: [i32; 2],
+}
+
+struct Entity {
+    coord : Vec2,
+    dest_size : Option<Vec2>,
+    velocity : Vec2,
+}
+
+impl Entity{
     fn draw(&mut self){
-        self.rotation = self.rotation % 6.28;
-        let draw_param = DrawTextureParams{
+        let _draw_param = DrawTextureParams{
             dest_size: self.dest_size,
-            rotation : self.rotation,
             ..Default::default()
         };
+        draw_rectangle(self.coord.x, self.coord.y, self.dest_size.unwrap().x, self.dest_size.unwrap().y, PURPLE);
+        
         // we use let Some ... because else we have to use clone or a lifetime to have texture in the function
-        draw_texture_ex(self.texture, self.coord.x, self.coord.y, WHITE, draw_param);
+        // draw_texture_ex(self.texture, self.coord.x, self.coord.y, WHITE, draw_param);
     }
-}
-impl Default for Entity{
-    fn default() -> Self {
-        Entity {
-            coord : Vec2 {x: 0., y: 0.},
-            texture : None,
-            rotation : 0.,
-            dest_size : None,
+    fn movement(&mut self){
+        if is_key_down(KeyCode::A){
+            self.coord.x -= self.velocity.x;
         }
+        if is_key_down(KeyCode::D){
+            self.coord.x += self.velocity.x;
+        }
+        if is_key_down(KeyCode::Space) && self.coord.y == 400.{
+            self.velocity.y = -12.;
+        }
+        if self.coord.y < 400.{
+            self.velocity.y +=0.5;
+        }
+        self.coord.y += self.velocity.y;
     }
 }
 
-#[macroquad::main("tourne-et-retourne")]
+
+struct _Resource{
+    textures : Vec<Texture2D>,
+} 
+
+#[macroquad::main(window_conf)]
 async fn main() {
-    let mut theo = Entity {
-        texture : Some(load_texture("assets/theo.png").await.unwrap()),
-        dest_size: Some(vec2(500., 500.)),
-        ..Default::default()
+    let mut maho_shojo = Entity {
+        coord : vec2(100., 400.),
+        dest_size : Some(vec2(30., 50.)),
+        velocity : vec2(5., 0.),
     };
+    let file = load_string("assets/test.ldtk").await.unwrap();
+    let project: LDtkProject = serde_json::from_str(&file).unwrap();
+    let level = &project.levels[0];
+    let layers = level.layer_instances.as_ref().unwrap();
+
+    
     loop {
         clear_background(SKYBLUE);
-        theo.rotation = theo.rotation + 0.1;
-        theo.draw();
+        maho_shojo.movement();
+        maho_shojo.draw();
+        for layer in layers {
+            if layer.identifier == "Base" {
+                for tile in &layer.tiles {
+                    let x = tile.position[0] as f32;
+                    let y = tile.position[1] as f32;
+                    draw_rectangle(x, y, 16.0, 16.0, DARKGREEN);
+                }
+            }
+        }
         next_frame().await; 
     }
 }
