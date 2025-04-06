644a6e1dc1dcb6b07a463dd12c3a00f777a3f75b
Aonfu <noafouich.etude@protonmail.com>
2025-04-06 16:23:44 +0800

    we continue : )

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::HashSet;
 use macroquad::prelude::*;
 use serde::Deserialize;
-use serde_json::{json,Value};
+
+const TILE_SIZE: i32 = 16;
+
 
 fn window_conf() -> Conf {
     Conf {
@@ -39,71 +42,98 @@ struct LDtkTile {
     position: [i32; 2],
 }
 
-struct Entity {
-    coord : Vec2,
-    dest_size : Option<Vec2>,
-    velocity : Vec2,
+struct Player {
+    hitbox : Rect,
+    speed : f32,
+    gravity : f32
 }
 
-impl Entity{
+impl Player{
     fn draw(&mut self){
         let _draw_param = DrawTextureParams{
-            dest_size: self.dest_size,
+            dest_size: Some(vec2(self.hitbox.w,self.hitbox.h)),
             ..Default::default()
         };
-        draw_rectangle(self.coord.x, self.coord.y, self.dest_size.unwrap().x, self.dest_size.unwrap().y, PURPLE);
+        draw_rectangle(self.hitbox.x, self.hitbox.y, self.hitbox.w, self.hitbox.h, PURPLE);
         
         // we use let Some ... because else we have to use clone or a lifetime to have texture in the function
         // draw_texture_ex(self.texture, self.coord.x, self.coord.y, WHITE, draw_param);
     }
-    fn movement(&mut self){
+    fn action(&mut self, map : &HashSet<[i32;2]>){
         if is_key_down(KeyCode::A){
-            self.coord.x -= self.velocity.x;
+            let temp = self.hitbox.x as i32 - self.speed as i32;
+            if map.contains(&[temp - temp % 16, 15*16]){
+                self.hitbox.x = (temp - temp % 16 + 16) as f32
+            }else {
+                self.hitbox.x = temp as f32
+            }
         }
         if is_key_down(KeyCode::D){
-            self.coord.x += self.velocity.x;
+            let temp = self.hitbox.x as i32 + self.hitbox.w as i32 + self.speed as i32;
+            if map.contains(&[temp - temp % 16, 15*16]){
+                self.hitbox.x = (temp - self.hitbox.w as i32 - temp % 16) as f32
+            }else {
+                self.hitbox.x = temp as f32 - self.hitbox.w
+            }
         }
-        if is_key_down(KeyCode::Space) && self.coord.y == 400.{
-            self.velocity.y = -12.;
+        if is_key_down(KeyCode::Space){
+            self.gravity = -6.;
         }
-        if self.coord.y < 400.{
-            self.velocity.y +=0.5;
-        }
-        self.coord.y += self.velocity.y;
+        self.hitbox.y += self.gravity;
     }
 }
-
 
 struct _Resource{
     textures : Vec<Texture2D>,
 } 
 
+fn lerp(a: f32, b: f32, t: f32) -> f32 {
+    a + (b - a) * t
+}
+
+fn lerp_vec2(a: Vec2, b: Vec2, t: f32) -> Vec2 {
+    vec2(
+        lerp(a.x, b.x, t),
+        lerp(a.y, b.y, t),
+    )
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut maho_shojo = Entity {
-        coord : vec2(100., 400.),
-        dest_size : Some(vec2(30., 50.)),
-        velocity : vec2(5., 0.),
+    let mut camera = Camera2D {
+        target: vec2(100.0, 100.0),
+        zoom: vec2(2.0 / screen_width(), 2.0 / screen_height()), 
+        ..Default::default()
     };
+    let mut maho_shojo = Player {
+        hitbox : Rect::new(2.*16., 16.*16.-24.,20., 24.),
+        speed : 5.,
+        gravity : 0.,
+    };
+
     let file = load_string("assets/test.ldtk").await.unwrap();
     let project: LDtkProject = serde_json::from_str(&file).unwrap();
     let level = &project.levels[0];
     let layers = level.layer_instances.as_ref().unwrap();
-
+    let mut map = HashSet::new();
+    layers.iter()
+    .find(|layer| layer.identifier == "Base")
+    .unwrap().tiles.iter()
+    .for_each(|tile| {map.insert(tile.position);} );
     
     loop {
         clear_background(SKYBLUE);
-        maho_shojo.movement();
+
+        camera.target = lerp_vec2(camera.target, vec2(maho_shojo.hitbox.x,maho_shojo.hitbox.y), 0.1); // ← version smooth
+        set_camera(&camera);
+        maho_shojo.action(&map);
         maho_shojo.draw();
-        for layer in layers {
-            if layer.identifier == "Base" {
-                for tile in &layer.tiles {
-                    let x = tile.position[0] as f32;
-                    let y = tile.position[1] as f32;
-                    draw_rectangle(x, y, 16.0, 16.0, DARKGREEN);
-                }
-            }
-        }
+        
+        layers.iter().find(|layer| layer.identifier == "Base").unwrap()
+        .tiles.iter()
+        .for_each(|tile| 
+            draw_rectangle(tile.position[0] as f32, tile.position[1] as f32, 16., 16., DARKGREEN));
         next_frame().await; 
+
     }
 }
